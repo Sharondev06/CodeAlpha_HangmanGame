91a2c2422867bfabb4dc3f0a1b33e53e409931f8
Sharondev06 <sharonikhidedara@gmail.com>
2026-05-17 18:12:07 +0100

    Add hint feature to Hangman Game

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ choose = random.choice(words).lower() #.lower()converts any string to lowerase.
 lifespan = 6
 guessed = []
 game_over = False
+hint_used = False
 
 print("Welcome to Hangman!")
 time.sleep(2)
 print("Get Ready")
+time.sleep(2)
+print("You can only get one hint when you type the magic word 'Hint' :)")
 time.sleep(2)
 while lifespan > 0 and not game_over:
     print(f"Lives remaining: {lifespan}")
@@ -19,17 +22,33 @@ while lifespan > 0 and not game_over:
             print("_", end=" ")   #if no- show _
     print()
     guess = input("Guess a letter: ").lower()  #the way its indented means its under the while loop block and using input() instead of print() is like a two way, it would display the Guess the letter and still allow input from the user unlike print() that just displays.
-    if guess in guessed:
-        print("you already guessed this letter")
-    else:
-        guessed.append(guess)
-        if guess in choose:
-            print("Great job")
-            if all(letter in guessed for letter in choose):
-                game_over = True
+    if guess == "hint":
+        if hint_used:
+            print("you have already used your hint")
+            print()
         else:
-            lifespan = lifespan - 1
-            print("Wrong guess!")
+            for letter in choose:
+                if letter not in guessed:
+                    print(f"Hint: The word contains '{letter}'")
+                    print()
+                    guessed.append(letter)
+                    hint_used = True
+                    break #break immediately stops a loop when called
+    else:
+        if guess in guessed:
+            print("you already guessed this letter")
+            print()
+        else:
+            guessed.append(guess)
+            if guess in choose:
+                print("Great job")
+                print()
+                if all(letter in guessed for letter in choose):
+                    game_over = True
+            else:
+                lifespan = lifespan - 1
+                print("Wrong guess!")
+                print()
 if lifespan == 0: #== compares two values while = assigns a value.
     print("YOU LOST")
 else:
